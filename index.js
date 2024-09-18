1db06cc6830773ac15ff9e9524b232f9d2f2b62a
Abdur Rahman Bilal <aramb@aramservices.com>
2024-09-18 11:07:02 -0400

    Refactor API response handling and add loading state to getSurahInfo button

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 let response;
 async function getSurahInfo() {
   try {
+    //  add 'is-loading' class to #getSurahInfo
+    document.querySelector("#getSurahInfo").classList.add("is-loading");
     response = await axios.get("http://api.alquran.cloud/v1/meta");
-    response = response.data;
-    document.querySelector(".api-response").innerText = JSON.stringify(response.data.surahs.references[0]);
-    document.querySelector("#response-visual").innerText = `Ayahs: ${response.data.ayahs.count}`
+    response = response.data.data;
+    document.querySelector(".api-response").innerText =
+      JSON.stringify(response);
+    for (let i = 0; i < response.surahs.references.length; i++) {
+      let surah = document.createElement("li");
+      surah.className = "box has-text-centered cell is-align-content-center is-flex-direction-row";
+      surah.innerHTML = `<h1 class="title is-size-4 ibm-plex-sans-arabic-thin surah-name">${response.surahs.references[i].name}</h1>\n                        <h2 class="title is-size-5 surah-name">${response.surahs.references[i].englishName}</h2>\n                        <h2 class="title is-size-5 surah-name">${response.surahs.references[i].englishNameTranslation}</h2>\n                        <h3 class="title is-size-6 surah-name">Ayahs: ${response.surahs.references[i].numberOfAyahs}</h3>`;
+      document.querySelector("#surahs").appendChild(surah);
+    }
+    document.querySelector("#getSurahInfo").classList.remove("is-loading");
   } catch (error) {
     document.querySelector(".api-error").innerText = error;
+    document.querySelector("#getSurahInfo").classList.remove("is-loading");
+    document.querySelector("#getSurahInfo").classList.remove("is-success");
+    document.querySelector("#getSurahInfo").classList.add("is-danger");
+    document.querySelector("#getSurahInfo").setAttribute("disabled", true);
+    document.querySelector("#getSurahInfo").innerText =
+      "API request failed. Please reload and try again.";
   }
 }
 
 document.querySelector("#getSurahInfo").addEventListener("click", () => {
-    getSurahInfo();
-    document.querySelector("#response-visual").innerText = `Ayahs: ${response.data.data.ayahs.count}`
+  getSurahInfo();
 });
